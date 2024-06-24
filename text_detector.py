19223af9a118eb2f45106dd3010b020f7f414dcd
pico <a.pico@posteo.de>
2024-06-24 16:08:00 +0200

    class for text detection in images

--- text_detector.py
+++ text_detector.py
@@ -13,15 +13,31 @@ from paddleocr import PaddleOCR, draw_ocr
 
 
 class TextDetector:
-    '''ss
+    '''
+    A class for detecting text in an image.
     '''
     def __init__(self, **kwargs):
+        ''' 
+        Initializes ocr detector 
+
+        Parameters:
+        kwargs(see PaddleOCR docs) 
+        '''
         
         # optical character recognition using paddleocr library
         self.ocr = PaddleOCR(rec=False, **kwargs)
         
         
-    def _preprocess_image(img):
+    def _preprocess_image(self, img):
+        ''' 
+        Applies Gaussianblur and binarizes input image
+
+        Parameters:
+        img (numpy array): opencv image
+
+        Returns:
+        black and white opencv image 
+        '''
         # Apply Gaussian blur
         blurred_image = cv2.GaussianBlur(img, (5, 5), 0)
         
@@ -30,7 +46,7 @@ class TextDetector:
         
         return binary_image
 
-    def __call__(self, img)
+    def __call__(self, img):
         ''' 
         Applies character detection to image.
 
@@ -44,4 +60,45 @@ class TextDetector:
         bounding_boxes = result[0]
         
         return bounding_boxes
+    
+    def draw(self, img, bboxes):
+        ''' 
+        Draws bounding boxes to image.
+
+        Parameters:
+        img (numpy array): opencv image
+        bboxes (numpy array): list of bounding boxes
+
+        Returns:
+        image with bounding boxes
+        '''
+        bounding_boxes_img = img.copy()
+        cv2.polylines(bounding_boxes_img, np.array(bboxes, dtype=np.int32), isClosed=True, color=(0, 255, 0), thickness=2)
+        
+        return cv2.cvtColor(bounding_boxes_img, cv2.COLOR_BGR2RGB)
+    
+    def crop_bounding_boxes(self, img, bboxes)
+        ''' 
+        Crops bounding boxes from image.
+
+        Parameters:
+        img (numpy array): opencv image
+        bboxes (numpy array): list of bounding boxes
+
+        Returns:
+        list of cropped images
+        '''
+        cropped_images = []
+
+        for box in bboxes:
+            box = np.array(box, dtype=np.int32)
+            # Get the bounding rectangle (minimum area rectangle) from the polygonal points
+            x, y, w, h = cv2.boundingRect(box)
+            # Crop the region of interest (ROI) from the image
+            cropped_image = img[y:y+h, x:x+w]
+            # Append the cropped image to the list
+            cropped_images.append(cropped_image)
+            
+        return cropped_images
+        
     
